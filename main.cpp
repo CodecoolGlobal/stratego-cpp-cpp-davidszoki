e5b607dd7a4c32ff61a2bb3341541113fe059df8
davidszoki <davidszokoloczi@gmail.com>
2024-09-24 15:49:41 +0200

    Make an example for Unit how it is working

--- main.cpp
+++ main.cpp
@@ -2,23 +2,31 @@
 #include <filesystem>
 #include "UI/Texture.h"
 #include "UI/UI.h"
-
+#include "Units/Marshal.h"
 using namespace std;
 
 int main(int, char **) {
-    UI ui;
-    ui.init();
-    auto path = filesystem::current_path().parent_path();
-    path /= "Resources";
-    string pathStr = path.u8string();
-    Texture battlefield = ui.loadBMP(pathStr + "/Battlefield.bmp");
-    bool quit = false;
-    // Main loop
-    while (!quit) {
-        quit = ui.handleEvent();
-        SDL_RenderClear(ui.getRenderer());
-        battlefield.render(ui.getRenderer(), nullptr);
-        SDL_RenderPresent(ui.getRenderer());
-    }
+    Position position = Position(10, 12);
+
+    Unit marshal(Marshal,none,position);
+
+    marshal.setPLayer(Red);
+
+    marshal.getPosition().printPosition();
+
+    // UI ui;
+    // ui.init();
+    // auto path = filesystem::current_path().parent_path();
+    // path /= "Resources";
+    // string pathStr = path.u8string();
+    // Texture battlefield = ui.loadBMP(pathStr + "/Battlefield.bmp");
+    // bool quit = false;
+    // // Main loop
+    // while (!quit) {
+    //     quit = ui.handleEvent();
+    //     SDL_RenderClear(ui.getRenderer());
+    //     battlefield.render(ui.getRenderer(), nullptr);
+    //     SDL_RenderPresent(ui.getRenderer());
+    // }
     return 0;
 }
