4c8fe99dfd0c45b777cf6e856de1b90526d0097f
kC0d3 <kC0d3@github.com>
2024-09-25 11:16:09 +0200

    style: Refactor format

--- UI/Texture.cpp
+++ UI/Texture.cpp
@@ -1,16 +1,13 @@
 #include "Texture.h"
 
-Texture::Texture(SDL_Texture* texture)
-{
-  this->texture = texture;
+Texture::Texture(SDL_Texture *texture) {
+    this->texture = texture;
 }
 
-Texture::~Texture()
-{
-  if(texture) SDL_DestroyTexture(texture);
+Texture::~Texture() {
+    if (texture) SDL_DestroyTexture(texture);
 }
 
-void Texture::render(SDL_Renderer* renderer, const SDL_Rect* pos)
-{
-  SDL_RenderCopy(renderer, texture, NULL, pos);
+void Texture::render(SDL_Renderer *renderer, const SDL_Rect *rect) {
+    SDL_RenderCopy(renderer, texture, NULL, rect);
 }
