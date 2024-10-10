467119fd3419d7a6773f8847b923a88aa0433a78
kC0d3 <kC0d3@github.com>
2024-10-10 18:39:49 +0200

    refactor: Change main method

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
-#include <vector>
-
-#include "GameLogic/GameLogic.h"
-#include "UI/Texture.h"
-#include "UI/UI.h"
-
+#include "Game.h"
 
 using namespace std;
 
 int main(int, char **) {
-    vector<string> players = {"Blue", "Red"};
-    GameLogic* gameLogic = new GameLogic();
-    (*gameLogic).run(players);
+    auto game = Game();
+    game.run();
     return 0;
 }
